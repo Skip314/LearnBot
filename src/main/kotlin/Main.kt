37ac1a08b9473c9b043e3ce07beed6a4d5782c77
mac <afanasevlad@hotmail.com>
2024-04-09 12:10:06 +0300

    stage 2 data class and quantityApprove

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,34 @@ package org.example
 
 import java.io.File
 
+data class Words(
+    val original: String,
+    val translate: String,
+    val quantityApprove: Int,
+)
+
 fun main() {
 
     val wordsFile: File = File("words.txt")
-    for(i in wordsFile.readLines()) {
-        println(i)
+    wordsFile.writeText("")
+    createStartWords()
+
+    val lines: List<String> = wordsFile.readLines()
+    wordsFile.writeText("")
+
+    val dictionaryWords: MutableMap<String, String> = mutableMapOf()
+
+    for (i in lines) {
+        val line = i.split("|")
+        val word = Words(original = line[0], translate = line[1], quantityApprove = line[2].toInt())
+        dictionaryWords[word.original] = word.translate
+        wordsFile.appendText("${word.original}|${word.translate}|${word.quantityApprove}\n")
     }
+    println(dictionaryWords)
+}
+fun createStartWords(){
+    val wordsFile: File = File("words.txt")
+    wordsFile.appendText("hello|привет|0\n")
+    wordsFile.appendText("dog|собака|0\n")
+    wordsFile.appendText("cat|кошка|0\n")
 }
